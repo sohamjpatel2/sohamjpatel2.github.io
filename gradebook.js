01e117f74a9b36049c1a7283ec046a599dfaa4c3
sohamjpatel2 <sjpatel2@my.waketech.edu>
2025-07-14 19:49:10 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,9 @@
+function fetchGradeData() {
+  console.log("Fetching grade data...");
+}
+function populateGradebook(data) {
+  console.log("Populating gradebook with data", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
